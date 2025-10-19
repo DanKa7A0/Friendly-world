098692601488706884ee05a76bfde1ccdf811412
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-10-19 13:50:32 +0300

    Fix required keyword in user model

--- src/models/User.js
+++ src/models/User.js
@@ -3,13 +3,13 @@ import { Schema, model } from "mongoose";
 const userSchema = new Schema({
     email: {
         type: String
-        , require: [true, "Email is required"]
+        , required: [true, "Email is required"]
         , unique: [true, "Email already exist"]
     }
 
     , password: {
         type: String
-        , require: [true, "Password is required"]
+        , required: [true, "Password is required"]
     }
 });
 
