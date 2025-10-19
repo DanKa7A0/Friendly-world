0efb0beb09b5657ee61dd785f0b30f6fc51e5b89
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-10-19 13:17:02 +0300

    Render register

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import homeController from "./controllers/homeController.js";
+import authController from "./controllers/authController.js";
 
 const router = Router();
 
 router.use(homeController);
+router.use(authController);
 
 export default router;
