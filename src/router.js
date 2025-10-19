0f09cb29113df6f6d0a6e9a6e7ba649f6180691e
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-10-19 22:00:17 +0300

    Implement render create animal

--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import homeController from "./controllers/homeController.js";
 import authController from "./controllers/authController.js";
+import animalController from "./controllers/animalController.js";
 
 const router = Router();
 
 router.use(homeController);
 router.use(authController);
+router.use("/animals", animalController);
 
 router.get("*splat", (req, res) => {
     res.render("404");
